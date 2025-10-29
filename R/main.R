d458c2e24aead745556ae77d03bec6373b136e72
adrianatovar <odris2017@gmail.com>
2025-10-29 11:06:31 +0100

    pinus nigra

--- R/main.R
+++ R/main.R
@@ -20,23 +20,31 @@ ifn3_cat <- rbind(readRDS("data-raw/ifn3_08.rds"),
 #     id_unique_code = id_unique_
 #   )
 
-parcelas_pinus_sylvestris <- read_delim(
-  "data-raw/parcelas_pinus_sylvestris.csv", 
-  delim = ";", escape_double = FALSE, trim_ws = TRUE) |> 
-    dplyr::rename(
-      id_unique_code = id_unique_
-    )
+# parcelas_pinus_sylvestris <- read_delim(
+#   "data-raw/parcelas_pinus_sylvestris.csv", 
+#   delim = ";", escape_double = FALSE, trim_ws = TRUE) |> 
+#     dplyr::rename(
+#       id_unique_code = id_unique_
+#     )
 
+# 
+# llista_pinus_nigra<- read_excel("llista_pinus_nigra_deboscat_TableToExcel.xlsx")
+# 
+# IDs<- llista_pinus_nigra$id_unique_code
 
-IDs<- c( "08_1338_NN_A1_A1",
-         "08_2144_NN_A1_A1",
-         "08_4048_xx_NN_A1",
-         "08_1883_NN_A1_A1",
-         "08_2211_xx_A4_A1",
-         "08_1879_NN_A1_A1",
-         "08_1890_NN_A1_A1",
-         "08_1146_NN_A1_A1"
-         )
+control_pinus_nigra <- read_excel("preseleccio_buffer_pinnig_control_TableToExcel.xlsx")
+
+IDs<- control_pinus_nigra$id_unique_code
+
+# 
+# 
+# IDs<- c(   "08_2616_NN_A1_A1", "08_2717_NN_A1_A1", "08_2737_NN_A1_A1" ,"08_2798_NN_A1_A1" ,"08_3005_NN_A1_A1",
+#            "08_3332_NN_A1_A1", "08_3457_NN_A1_A1", "17_1635_NN_A1_A1" ,"17_1652_NN_A1_A1" ,"25_3212_NN_A1_A1",
+#            "43_0521_NN_A1_A1", "43_0982_NN_A1_A1", "43_1108_NN_A1_A1" ,"43_2028_xx_NN_A1" ,"43_2064_xx_NN_A1",
+#            "08_1292_xx_A4_A1", "08_1355_NN_A1_A1", "08_1879_NN_A1_A1", "17_0557_NN_A1_A1" ,"17_0920_NN_A1_A1",
+#           "17_1843_NN_A1_A1", "25_0960_NN_A1_A1" ,"25_1066_NN_A1_A1", "25_1072_NN_A1_A1" ,"25_1862_NN_A1_A1",
+#          "25_2427_NN_A1_A1", "25_4349_NN_A1_A1", "43_0594_xx_A4_A1", "43_0610_NN_A1_A1" ,"43_0697_NN_A1_A1"
+#          )
 
 # ids_25 <- parcelas_pinus_sylvestris$id_unique_code[grepl("^25", parcelas_pinus_sylvestris$id_unique_code)]
 # 
